c8b9b5a7779bfad3fa6883662496eef85d2f9e31
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-01 18:35:06 +0200

    Add cluster migration

---
+++ database/migrations/2024_09_01_163229_create_clusters_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clusters', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("token");
+            $table->string("description");
+            $table->string("endpoint")->default("https://localhost:1234");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clusters');
+    }
+};
