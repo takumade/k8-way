96b1647c52acd6ed2c2442f6f816bd3ffcc779fb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-01 23:52:29 +0200

    Fix script issue

--- run-k8s-swagger.sh
+++ run-k8s-swagger.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run --rm -p 9000:8080 -e SWAGGER_JSON=/k8s-openapi-v2.json \
--v $(pwd)/k8s-openapi-v2.json:/k8s-openapi-v2.json \
-swaggerapi/swagger-ui
+docker run --rm \
+  -p 9000:8080 \
+  -e SWAGGER_JSON=/k8s-openapi-v2.json \
+  -v "$(pwd)/k8s-openapi-v2.json:/k8s-openapi-v2.json" \
+  swaggerapi/swagger-ui
