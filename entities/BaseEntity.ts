44eb94cc73d0aaea34137331f3da9c6a10207aaf
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-07 17:37:08 +0200

    Add base entity

---
+++ entities/BaseEntity.ts
@@ -0,0 +1,26 @@
+const { EntitySchema } = require("@mikro-orm/core");
+class BaseEntity {
+
+  createdAt 
+  updatedAt
+    
+  constructor() {
+    this.createdAt = new Date();
+    this.updatedAt = new Date();
+  }
+}
+const schema = new EntitySchema({
+  name: "BaseEntity",
+  abstract: true,
+  properties: {
+    id: { primary: true, type: "number" },
+    createdAt: { type: "Date" },
+    updatedAt: { type: "Date", onUpdate: () => new Date() },
+  },
+});
+module.exports = {
+  BaseEntity,
+  entity: BaseEntity,
+  schema,
+  label: "BaseEntity",
+};
