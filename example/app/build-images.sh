48f73ddcd4305c9620f14131a07433bf73f5e0ea
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-07 17:04:50 +0200

    Add microk8s commands

--- example/app/build-images.sh
+++ example/app/build-images.sh
@@ -1,8 +1,12 @@
  
-# For frontend
- 
-docker build -t frontend frontend/
- 
-# For backend
- 
-docker build -t backend backend/
+# Just build frontend and backend 
+# docker build -t frontend frontend/
+# docker build -t backend backend/
+
+
+# Build and push to microk8s
+sudo docker build -t localhost:32000/frontend:latest frontend 
+sudo docker build -t localhost:32000/backend:latest backend 
+
+sudo docker push localhost:32000/frontend:latest
+sudo docker push localhost:32000/backend:latest
