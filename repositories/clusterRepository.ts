b08e8502ef4fa4bb5e89fb4a6752b4e6b2a04760
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 19:59:43 +0200

    find Cluster by id

--- repositories/clusterRepository.ts
+++ repositories/clusterRepository.ts
@@ -1,6 +1,14 @@
 import { db } from "@/database";
 import { Cluster, ClusterUpdate, NewCluster } from "@/entities";
 
+
+export async function findClusterById(id: number) {
+    return await db.selectFrom('cluster')
+      .where('id', '=', id)
+      .selectAll()
+      .executeTakeFirst()
+  }
+
 export async function createCluster(data: NewCluster){
     return await db.insertInto('cluster')
                     .values(data)
