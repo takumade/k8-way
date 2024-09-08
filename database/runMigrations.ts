77ff4b75439c29c42bd5410a6ab98cad0d5c34c8
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 21:15:42 +0200

    Add runMIgration file

---
+++ database/runMigrations.ts
@@ -0,0 +1,45 @@
+import * as path from 'path'
+import { Database } from "./entities" // this is the Database interface we defined earlier
+
+import { promises as fs } from 'fs'
+import {
+  Kysely,
+  Migrator,
+  SqliteDialect,
+  FileMigrationProvider,
+} from 'kysely'
+import { db } from '@/database'
+
+async function migrateToLatest() {
+
+
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      // This needs to be an absolute path.
+      migrationFolder:  path.resolve('./database/migrations'),
+    }),
+  })
+
+  const { error, results } = await migrator.migrateToLatest()
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`)
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`)
+    }
+  })
+
+  if (error) {
+    console.error('failed to migrate')
+    console.error(error)
+    process.exit(1)
+  }
+
+  await db.destroy()
+}
+
+migrateToLatest()
