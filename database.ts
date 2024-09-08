424b2833cfa03c51a20a0a301998c8918b4fca15
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 22:32:47 +0200

    Add db name

--- database.ts
+++ database.ts
@@ -1,17 +1,17 @@
-import { Database } from "./database/entities" // this is the Database interface we defined earlier
+import { Database as DatabaseEntities } from "./database/entities" // this is the Database interface we defined earlier
 
 //@ts-ignore
-import SQLite from 'better-sqlite3'
+import Database from 'better-sqlite3'
 import { Kysely, SqliteDialect } from 'kysely'
 
 const dialect = new SqliteDialect({
-  database: new SQLite(),
+  database: new Database("db.sqlite"),
 })
 
 // Database interface is passed to Kysely's constructor, and from now on, Kysely 
 // knows your database structure.
 // Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
 // to communicate with your database.
-export const db = new Kysely<Database>({
+export const db = new Kysely<DatabaseEntities>({
   dialect,
 })
