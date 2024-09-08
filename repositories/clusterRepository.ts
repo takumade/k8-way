b23bd3fdfce6f8f15eb4f656f4861201f0f0516a
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 22:33:37 +0200

    Log some text

--- repositories/clusterRepository.ts
+++ repositories/clusterRepository.ts
@@ -6,28 +6,33 @@ import { Cluster, ClusterUpdate, NewCluster } from "@/database/entities";
 
 export async function findClusterById(id: number) {
     return await db.selectFrom('cluster')
-      .where('id', '=', id)
-      .selectAll()
-      .executeTakeFirst()
-  }
+        .where('id', '=', id)
+        .selectAll()
+        .executeTakeFirst()
+}
 
-export async function createCluster(data: NewCluster){
+export async function createCluster(data: NewCluster) {
+
+    console.log("Add cluster...")
     return await db.insertInto('cluster')
-                    .values(data)
-                    .returningAll()
-                    .executeTakeFirstOrThrow()
+        .values(data)
+        .returningAll()
+        .executeTakeFirstOrThrow()
+
 }
 
 
 export async function updateCluster(id: number, updateWith: ClusterUpdate) {
-    await db.updateTable('cluster')
-            .set(updateWith)
-            .where('id', '=', id).execute()
-  }
+    console.log("Update cluster...")
+    return await db.updateTable('cluster')
+        .set(updateWith)
+        .where('id', '=', id).execute()
+
+}
 
 
-  export async function deleteCluster(id: number) {
+export async function deleteCluster(id: number) {
     return await db.deleteFrom('cluster').where('id', '=', id)
-      .returningAll()
-      .executeTakeFirst()
-  }
+        .returningAll()
+        .executeTakeFirst()
+}
