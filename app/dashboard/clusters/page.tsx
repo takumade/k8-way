659000efc120d548e28945dbbf0d797685262ed0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 23:25:58 +0200

    Get clusters to client

--- app/dashboard/clusters/page.tsx
+++ app/dashboard/clusters/page.tsx
@@ -1,18 +1,22 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import { ClusterClient } from '@/components/tables/cluster-tables/client';
-import { clusters } from '@/constants/data';
+import { Cluster } from '@/database/entities';
+import { getClusters } from '@/repositories/clusterRepository';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
   { title: 'Cluster', link: '/dashboard/clusters' }
 ];
-export default function page() {
+export default async function page() {
+
+  let clusters = await getClusters()
+
   return (
     <PageContainer>
       <div className="space-y-2">
         <Breadcrumbs items={breadcrumbItems} />
-        <ClusterClient data={clusters} />
+        <ClusterClient data={clusters as Cluster[] | undefined} />
       </div>
     </PageContainer>
   );
