35926311b195e5912af3d5a90886417a725fdaf3
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-08 23:25:40 +0200

    comment out menus

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { getClusters } from '@/repositories/clusterRepository';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -8,14 +9,16 @@ export const metadata: Metadata = {
   description: 'Basic dashboard with Next.js and Shadcn'
 };
 
-export default function DashboardLayout({
+export default async  function DashboardLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
+
+  let clusters = await getClusters()
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar clusters={clusters}/>
       <main className="w-full flex-1 overflow-hidden">
         <Header />
         {children}
