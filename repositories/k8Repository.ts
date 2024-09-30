6e7d174d760b7f289191fe469b3424dd816b3572
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-01 00:05:55 +0200

    get volumes

--- repositories/k8Repository.ts
+++ repositories/k8Repository.ts
@@ -75,4 +75,8 @@ export async function getNamespaces() {
     return await getResource(myCluster, 'namespaces', '')
 }
 
+export async function getVolumes() {
+    return await getResource(myCluster, 'persistentvolumes', '')
+}
+
 
