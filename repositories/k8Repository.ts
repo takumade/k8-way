0b1318d26721bfa6219ebc0bf5c925829193b3c0
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-09-30 23:48:59 +0200

    Refactor

--- repositories/k8Repository.ts
+++ repositories/k8Repository.ts
@@ -16,7 +16,7 @@ const myCluster:Cluster = {
     token:  " eyJhbGciOiJSUzI1NiIsImtpZCI6InlHQmdCYURjSjh2Mk5CUWdwNkxxNEdndnJSVnpzeTYzVDNXVEFnWmFZOTAifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjIl0sImV4cCI6MTcyNzczNTAyNCwiaWF0IjoxNzI3NzMxNDI0LCJpc3MiOiJodHRwczovL2t1YmVybmV0ZXMuZGVmYXVsdC5zdmMiLCJqdGkiOiI4MTU0NWVlOC1kMmYwLTQzYWMtYjE1Ni1hYWI0YjYyNjMxYmUiLCJrdWJlcm5ldGVzLmlvIjp7Im5hbWVzcGFjZSI6ImRlZmF1bHQiLCJzZXJ2aWNlYWNjb3VudCI6eyJuYW1lIjoiZGVmYXVsdCIsInVpZCI6Ijk3NmIwOWJmLTYxM2QtNGNmMS05MzQ1LTIyMjExYTg2MmU5MSJ9fSwibmJmIjoxNzI3NzMxNDI0LCJzdWIiOiJzeXN0ZW06c2VydmljZWFjY291bnQ6ZGVmYXVsdDpkZWZhdWx0In0.SrlGZ88nJs4hsfd_C1Gq2UMtk6sU1gLt3otIQo9zJh_yHdnXeeTCZBBsNxmTfgdsLwDvVC3gFkxPR0_88u2tvHy0EHUvaXE0TuECxSHJlTS7lzEiK8wy13sU8hXbhrFthpiEMUJR5NKYzwhx9dDDpcmiIjbJQqGRqT3alfWf2Kvn87k5nG3SAmc_GpQc4mG6ECzD3QkX_Dhrv8wuv-Kda8bsZHL38dTjonX6iKhDi8ZWCORzx0Ot52bGawldUgZcbcNgmVlTtV67VWs2CoTmTWI9FleYfmPQ7oDc28aEdELXbTubGun5GVGnZZhzN8nu2R0kNJEFUUzox1lpbCeA_Q"
 }
 
-async function getResources(cluster:Cluster, resource:string, namespace:string = "") {
+async function getResource(cluster:Cluster, resource:string, namespace:string = "") {
     let headers = generateHeaders(cluster.token)
 
     let resourceUrl = `https://${cluster.endpoint}/api/v1/${resource}`
@@ -41,35 +41,11 @@ async function getResources(cluster:Cluster, resource:string, namespace:string =
 
 
 export async function getNodes() {
-    let headers = genHeaders(token)
-    let results = await fetch('https://127.0.0.1:16443/api/v1/nodes', {
-        method: 'GET',
-        headers: headers
-    })
-
-    let data = await results.json()
-
-    if (data.items) {
-        return data.items
-    }
-
-    return []
+    return await getResource(myCluster, 'nodes')
 }
 
 export async function getServices() {
-    let headers = genHeaders(token)
-    let results = await fetch('https://127.0.0.1:16443/api/v1/services', {
-        method: 'GET',
-        headers: headers
-    })
-
-    let data = await results.json()
-
-    if (data.items) {
-        return data.items
-    }
-
-    return []
+    return await getResource(myCluster, 'services')
 }
 
 
