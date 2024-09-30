84826ff02a6bd426b1d099072a849e5da6fb1fa8
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-01 01:27:00 +0200

    fetch nodes

--- app/dashboard/resources/nodes/page.tsx
+++ app/dashboard/resources/nodes/page.tsx
@@ -1,8 +1,10 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
 import { ClusterClient } from '@/components/tables/cluster-tables/client';
+import { NodesClient } from '@/components/tables/nodes-tables/client';
 import { Cluster } from '@/database/entities';
 import { getClusters } from '@/repositories/clusterRepository';
+import { getNodes } from '@/repositories/k8Repository';
 
 const breadcrumbItems = [
   { title: 'Dashboard', link: '/dashboard' },
@@ -10,13 +12,13 @@ const breadcrumbItems = [
 ];
 export default async function page() {
 
-  let clusters = await getClusters()
-
+  let nodes = await getNodes()
+  
   return (
     <PageContainer>
       <div className="space-y-2">
         <Breadcrumbs items={breadcrumbItems} />
-        <ClusterClient data={clusters as Cluster[] | undefined} />
+        <NodesClient data={nodes} />
       </div>
     </PageContainer>
   );
