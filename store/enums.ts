7f88e3b1c9eb4c334fae2254a4799b12042a7e32
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-04 00:15:47 +0200

    Add enums

---
+++ store/enums.ts
@@ -0,0 +1,12 @@
+enum ClusterActionType {
+    SELECT_CLUSTER = 'SELECT_CLUSTER',
+    ADD_CLUSTERS = 'ADD_CLUSTERS'
+  }
+  
+enum NamespaceActionType {
+    SELECT_NAMESPACE = 'SELECT_NAMESPACE',
+    ADD_NAMESPACES = 'ADD_NAMESPACES'
+  }
+
+
+  export { ClusterActionType, NamespaceActionType }
