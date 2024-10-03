3b659722f8610834ba53d83cba332597637be5b5
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-03 23:21:25 +0200

    Add cluster reduicer

--- lib/store.ts
+++ lib/store.ts
@@ -1,30 +1,31 @@
 
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from './counterReducer'
 import { createAction, createReducer } from '@reduxjs/toolkit'
+
+
+
+const initialState = { 
+  selectedCluster: null,
+  clusters: []
+}
+
+const selectCluster = createAction('counter/increment')
+const addClusters = createAction('counter/incrementByAmount')
+
+const clusterReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(selectCluster, (state, action) => {
+      state.selectedCluster = action.payload as any
+    })
+    .addCase(addClusters, (state, action) => {
+      state.clusters = action.payload as any
+    })
+})
 
 const store = configureStore({
   reducer: {
     cluster: clusterReducer
   },
-})
-
-
-const initialState = { 
-  selectedCluster: 0,
-  clusters: []
-}
-
-
-
-const clusterReducer = createReducer(initialState, (builder) => {
-  builder
-    .addCase(increment, (state, action) => {
-      state.value++
-    })
-    .addCase(incrementByAmount, (state, action) => {
-      state.value += action.payload
-    })
 })
 
 
