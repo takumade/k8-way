f6ed594953f623fdd9c26b8cb0c52b18a7c21cb7
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-03 23:15:24 +0200

    Add skeleton

--- lib/store.ts
+++ lib/store.ts
@@ -1,1 +1,12 @@
 
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './counterReducer'
+import { createAction, createReducer } from '@reduxjs/toolkit'
+
+const store = configureStore({
+  reducer: {
+    cluster: clusterReducer
+  },
+})
+
+export default store
