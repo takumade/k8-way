28ba4c6eefc30920147d63c18f95ca8b28f75067
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-05 20:26:25 +0200

    Show clusters for mobile setup

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -2,13 +2,15 @@ import ThemeToggle from '@/components/layout/ThemeToggle/theme-toggle';
 import { cn } from '@/lib/utils';
 import { MobileSidebar } from './mobile-sidebar';
 import { UserNav } from './user-nav';
+import { getClusters } from '@/repositories/clusterRepository';
 
-export default function Header() {
+export default async function Header() {
+  let clusters = await getClusters()
   return (
     <header className="sticky inset-x-0 top-0 w-full">
       <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
         <div className={cn('block lg:!hidden')}>
-          <MobileSidebar />
+          <MobileSidebar clusters={clusters} />
         </div>
         <div className="flex items-center gap-2">
           <UserNav />
