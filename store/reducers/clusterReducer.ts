502fd47670d11136e387b242d9f8e15eee2af1f2
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-05 21:08:19 +0200

    Refacrtor to use slices

---
+++ store/reducers/clusterReducer.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateCluster = {
+    selectedCluster: null,
+    clusters: []
+}
+
+const clusterSlice = createSlice({
+    name: 'cluster',
+    initialState: initialStateCluster,
+    reducers: {
+        selectCluster: (state, action) => {
+            console.log("Payload: ", action)
+            state.selectedCluster = action.payload;
+        },
+        addClusters: (state, action) => {
+            state.clusters = action.payload;
+        }
+    }
+});
+
+export const { selectCluster, addClusters } = clusterSlice.actions;
+export const clusterReducer = clusterSlice.reducer;
---
+++ store/reducers/namespaceReducer.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialStateNamespace = {
+    selectedNamespace: null,
+    namespaces: []
+};
+
+const namespaceSlice = createSlice({
+    name: 'namespace',
+    initialState: initialStateNamespace,
+    reducers: {
+        selectNamespace(state, action) {
+            state.selectedNamespace = action.payload;
+        },
+        addNamespaces(state, action) {
+            state.namespaces = action.payload;
+        }
+    }
+});
+
+export const { selectNamespace, addNamespaces } = namespaceSlice.actions;
+export const namespaceReducer =  namespaceSlice.reducer;
