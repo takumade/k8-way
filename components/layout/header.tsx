e8f3cc6db375778325d5da4c259f7a54c6c865d1
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-06 21:28:40 +0200

    Fetch clusters and populate forms

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -5,12 +5,12 @@ import { UserNav } from './user-nav';
 import { getClusters } from '@/repositories/clusterRepository';
 
 export default async function Header() {
-  let clusters = await getClusters()
+
   return (
     <header className="sticky inset-x-0 top-0 w-full">
       <nav className="flex items-center justify-between px-4 py-2 md:justify-end">
         <div className={cn('block lg:!hidden')}>
-          <MobileSidebar clusters={clusters} />
+          <MobileSidebar />
         </div>
         <div className="flex items-center gap-2">
           <UserNav />
