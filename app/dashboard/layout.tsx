5dc470252d352c5338281f5832c3465916c0e9fb
Takunda Madechangu <madechangu.takunda@gmail.com>
2024-10-06 21:28:53 +0200

    Remove clusters props

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import Header from '@/components/layout/header';
 import Sidebar from '@/components/layout/sidebar';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { getClusters } from '@/repositories/clusterRepository';
+import React from 'react';
 import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,16 +9,15 @@ export const metadata: Metadata = {
   description: 'Basic dashboard with Next.js and Shadcn'
 };
 
-export default async  function DashboardLayout({
+export default async function DashboardLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
 
-  let clusters = await getClusters()
   return (
     <div className="flex">
-      <Sidebar clusters={clusters}/>
+      <Sidebar />
       <main className="w-full flex-1 overflow-hidden">
         <Header />
         {children}
